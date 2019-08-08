d47875f131802939986109b4ff281de05f84a9c9
Superadm1n <kowalkyl@gmail.com>
2019-08-08 14:01:08 -0500

    Added tests for Global Credentilas

---
+++ tests/test_v1_1/NetworkDiscovery/test_GlobalCredentials.py
@@ -0,0 +1,199 @@
+from tests.test_v1_1.NetworkDiscovery.testable_objects import TestableGlobalCredentials as GlobalCredentials
+from unittest import TestCase
+
+
+class credential_sub_type(TestCase):
+
+    def setUp(self) -> None:
+        self.instance = GlobalCredentials()
+        self.id_used = 'junk'
+        self.execution_id = self.instance.credential_sub_type(self.id_used)
+
+    def test_passes_proper_url(self):
+        self.assertEqual(self.instance.get_handler_passed_url[self.execution_id], '/global-credential/junk')
+
+    def test_calls_response_handler(self):
+        self.assertEqual(self.instance.response_handler_called, 1)
+
+
+class cli(TestCase):
+
+    def setUp(self) -> None:
+        self.instance = GlobalCredentials()
+        self.expected_url = '/global_credential'
+        self.execution_id = self.instance.cli()
+
+    def test_passes_proper_url(self):
+        self.assertEqual(self.instance.get_handler_passed_url[self.execution_id], '/global-credential')
+
+    def test_accepts_proper_kwargs(self):
+        acceptable_kwargs = {'sortBy': 'junk', 'order': 'junk'}
+        id = self.instance.cli(**acceptable_kwargs)
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[id], {'credentialSubType': 'CLI', 'order': 'junk', 'sortBy': 'junk'})
+
+    def test_exception_on_bad_kwarg(self):
+        bad_kwargs = {'bad_kwarg': 'junk', 'order': 'junk'}
+        with self.assertRaises(KeyError):
+            self.instance.cli(**bad_kwargs)
+
+    def test_calls_response_handler(self):
+        self.assertEqual(self.instance.response_handler_called, 1)
+
+    def test_always_passes_credentialSubType(self):
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[self.execution_id], {'credentialSubType': 'CLI'})
+
+
+class snmpv2_read(TestCase):
+    def setUp(self) -> None:
+        self.instance = GlobalCredentials()
+        self.expected_url = '/global_credential'
+        self.execution_id = self.instance.snmpv2_read()
+
+    def test_passes_proper_url(self):
+        self.assertEqual(self.instance.get_handler_passed_url[self.execution_id], '/global-credential')
+
+    def test_accepts_proper_kwargs(self):
+        acceptable_kwargs = {'sortBy': 'junk', 'order': 'junk'}
+        id = self.instance.snmpv2_read(**acceptable_kwargs)
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[id], {'credentialSubType': 'SNMPV2_READ_COMMUNITY', 'order': 'junk', 'sortBy': 'junk'})
+
+    def test_exception_on_bad_kwarg(self):
+        bad_kwargs = {'bad_kwarg': 'junk', 'order': 'junk'}
+        with self.assertRaises(KeyError):
+            self.instance.snmpv2_read(**bad_kwargs)
+
+    def test_calls_response_handler(self):
+        self.assertEqual(self.instance.response_handler_called, 1)
+
+    def test_always_passes_credentialSubType(self):
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[self.execution_id], {'credentialSubType': 'SNMPV2_READ_COMMUNITY'})
+
+
+class snmpv2_write(TestCase):
+    def setUp(self) -> None:
+        self.instance = GlobalCredentials()
+        self.expected_url = '/global_credential'
+        self.execution_id = self.instance.snmpv2_write()
+
+    def test_passes_proper_url(self):
+        self.assertEqual(self.instance.get_handler_passed_url[self.execution_id], '/global-credential')
+
+    def test_accepts_proper_kwargs(self):
+        acceptable_kwargs = {'sortBy': 'junk', 'order': 'junk'}
+        id = self.instance.snmpv2_write(**acceptable_kwargs)
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[id], {'credentialSubType': 'SNMPV2_WRITE_COMMUNITY', 'order': 'junk', 'sortBy': 'junk'})
+
+    def test_exception_on_bad_kwarg(self):
+        bad_kwargs = {'bad_kwarg': 'junk', 'order': 'junk'}
+        with self.assertRaises(KeyError):
+            self.instance.snmpv2_write(**bad_kwargs)
+
+    def test_calls_response_handler(self):
+        self.assertEqual(self.instance.response_handler_called, 1)
+
+    def test_always_passes_credentialSubType(self):
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[self.execution_id], {'credentialSubType': 'SNMPV2_WRITE_COMMUNITY'})
+
+
+class snmpv3(TestCase):
+    def setUp(self) -> None:
+        self.instance = GlobalCredentials()
+        self.expected_url = '/global_credential'
+        self.execution_id = self.instance.snmpv3()
+
+    def test_passes_proper_url(self):
+        self.assertEqual(self.instance.get_handler_passed_url[self.execution_id], '/global-credential')
+
+    def test_accepts_proper_kwargs(self):
+        acceptable_kwargs = {'sortBy': 'junk', 'order': 'junk'}
+        id = self.instance.snmpv3(**acceptable_kwargs)
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[id], {'credentialSubType': 'SNMPV3', 'order': 'junk', 'sortBy': 'junk'})
+
+    def test_exception_on_bad_kwarg(self):
+        bad_kwargs = {'bad_kwarg': 'junk', 'order': 'junk'}
+        with self.assertRaises(KeyError):
+            self.instance.snmpv3(**bad_kwargs)
+
+    def test_calls_response_handler(self):
+        self.assertEqual(self.instance.response_handler_called, 1)
+
+    def test_always_passes_credentialSubType(self):
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[self.execution_id], {'credentialSubType': 'SNMPV3'})
+
+
+class http_write(TestCase):
+    def setUp(self) -> None:
+        self.instance = GlobalCredentials()
+        self.expected_url = '/global_credential'
+        self.execution_id = self.instance.http_write()
+
+    def test_passes_proper_url(self):
+        self.assertEqual(self.instance.get_handler_passed_url[self.execution_id], '/global-credential')
+
+    def test_accepts_proper_kwargs(self):
+        acceptable_kwargs = {'sortBy': 'junk', 'order': 'junk'}
+        id = self.instance.http_write(**acceptable_kwargs)
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[id], {'credentialSubType': 'HTTP_WRITE', 'order': 'junk', 'sortBy': 'junk'})
+
+    def test_exception_on_bad_kwarg(self):
+        bad_kwargs = {'bad_kwarg': 'junk', 'order': 'junk'}
+        with self.assertRaises(KeyError):
+            self.instance.http_write(**bad_kwargs)
+
+    def test_calls_response_handler(self):
+        self.assertEqual(self.instance.response_handler_called, 1)
+
+    def test_always_passes_credentialSubType(self):
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[self.execution_id], {'credentialSubType': 'HTTP_WRITE'})
+
+
+class http_read(TestCase):
+    def setUp(self) -> None:
+        self.instance = GlobalCredentials()
+        self.expected_url = '/global_credential'
+        self.execution_id = self.instance.http_read()
+
+    def test_passes_proper_url(self):
+        self.assertEqual(self.instance.get_handler_passed_url[self.execution_id], '/global-credential')
+
+    def test_accepts_proper_kwargs(self):
+        acceptable_kwargs = {'sortBy': 'junk', 'order': 'junk'}
+        id = self.instance.http_read(**acceptable_kwargs)
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[id], {'credentialSubType': 'HTTP_READ', 'order': 'junk', 'sortBy': 'junk'})
+
+    def test_exception_on_bad_kwarg(self):
+        bad_kwargs = {'bad_kwarg': 'junk', 'order': 'junk'}
+        with self.assertRaises(KeyError):
+            self.instance.http_read(**bad_kwargs)
+
+    def test_calls_response_handler(self):
+        self.assertEqual(self.instance.response_handler_called, 1)
+
+    def test_always_passes_credentialSubType(self):
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[self.execution_id], {'credentialSubType': 'HTTP_READ'})
+
+
+class netconf(TestCase):
+    def setUp(self) -> None:
+        self.instance = GlobalCredentials()
+        self.expected_url = '/global_credential'
+        self.execution_id = self.instance.netconf()
+
+    def test_passes_proper_url(self):
+        self.assertEqual(self.instance.get_handler_passed_url[self.execution_id], '/global-credential')
+
+    def test_accepts_proper_kwargs(self):
+        acceptable_kwargs = {'sortBy': 'junk', 'order': 'junk'}
+        id = self.instance.netconf(**acceptable_kwargs)
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[id], {'credentialSubType': 'NETCONF', 'order': 'junk', 'sortBy': 'junk'})
+
+    def test_exception_on_bad_kwarg(self):
+        bad_kwargs = {'bad_kwarg': 'junk', 'order': 'junk'}
+        with self.assertRaises(KeyError):
+            self.instance.netconf(**bad_kwargs)
+
+    def test_calls_response_handler(self):
+        self.assertEqual(self.instance.response_handler_called, 1)
+
+    def test_always_passes_credentialSubType(self):
+        self.assertEqual(self.instance.get_handler_passed_url_paramenters[self.execution_id], {'credentialSubType': 'NETCONF'})
