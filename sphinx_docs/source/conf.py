d47898e5a6d18009289345c6e9e19c79a794d28c
Superadm1n <kowalkyl@gmail.com>
2019-08-09 14:50:40 -0500

    Added fix for readthedocs build error

--- sphinx_docs/source/conf.py
+++ sphinx_docs/source/conf.py
@@ -24,6 +24,7 @@ author = 'Kyle Kowalczyk'
 
 # The full version, including alpha/beta/rc tags
 release = __version__
+master_doc = 'index'
 
 
 # -- General configuration ---------------------------------------------------
